cccc09761db0d8f3ed0a36b6947a0a3501c967ac
Fajrikun Makalalag <S2210271@student.unklab.ac.id>
2025-03-11 23:42:16 +0800

    Buat Repository

---
+++ src/test/java/com/Project_backend/Group1/Group1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project_backend.Group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
